25a8e817a13c00daa8b2381bc2cd5a7466d390b9
Angus <angusp@angusp.com>
2021-11-16 18:56:52 -0800

    test: adds jest packages
    Adds jest testing runner and react testing library.
    Also adds the test action for pull requests
    
    closes #12

---
+++ pages/index.test.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import Home from './index';
+
+describe('<Home/>', () => {
+  it('renders correctly', () => {
+    expect.assertions(1);
+
+    render(<Home />);
+
+    const heading = screen.getByRole('heading', { name: /welcome to next\.js!/i });
+
+    expect(heading).toBeInTheDocument();
+  });
+});
