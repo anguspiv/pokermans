4d7f97354873f4554bbd52d26453905a267caea2
Angus <angusp@angusp.com>
2021-11-17 14:03:34 -0800

    chore: adds chakra-ui framework
    closes #14

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,14 @@
+import { ChakraProvider } from '@chakra-ui/react';
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 
 // eslint-disable-next-line react/function-component-definition
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <ChakraProvider>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 }
 
 export default MyApp;
