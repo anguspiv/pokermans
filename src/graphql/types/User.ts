f6087c35fead004aceec316a568f41c498a2476c
Angus <angusp@angusp.com>
2021-11-28 17:16:04 -0800

    build: adds vercel build command
    Adds the vercel build command to add prisma migrations to deployments
    
    re #16

--- src/graphql/types/User.ts
+++ src/graphql/types/User.ts
@@ -1,9 +1,21 @@
-import { objectType } from 'nexus';
+import { objectType, extendType } from 'nexus';
 
 // eslint-disable-next-line import/prefer-default-export
 export const User = objectType({
   name: 'User',
   definition(t) {
     t.string('id');
+  },
+});
+
+export const UsersQuery = extendType({
+  type: 'Query',
+  definition(t) {
+    t.nonNull.list.field('users', {
+      type: 'User',
+      resolve(_parent, _args, ctx) {
+        return ctx.prisma.user.findMany();
+      },
+    });
   },
 });
