afb88ba9a8d753b28785ca18ea2028fc46b18214
Angus <angusp@angusp.com>
2021-12-13 21:06:46 -0800

    feat: adds profile card component
    Adds a ProfileCard component
    
    re #47

---
+++ src/components/ProfileCard/ProfileCard.tsx
@@ -0,0 +1,61 @@
+import React from 'react';
+import { Grid, GridItem, Heading, Text, Avatar } from '@chakra-ui/react';
+
+const styles = {
+  nickname: {
+    '&:before': {
+      content: 'open-quote',
+    },
+    '&:after': {
+      content: 'close-quote',
+    },
+  },
+};
+
+export function ProfileCard({
+  firstName = '',
+  lastName = '',
+  nickname = '',
+  email = '',
+  image = '',
+}: {
+  firstName: string;
+  lastName: string;
+  nickname: string;
+  email: string;
+  image: string;
+}) {
+  return (
+    <Grid
+      bg="white"
+      p={4}
+      borderWidth="thin"
+      borderRadius="lg"
+      data-testid="profile-card"
+      display="grid"
+      gap={4}
+      templateRows="auto"
+      templateColumns="repeat(2, auto)"
+      alignItems="center"
+      justifyContent="start"
+      maxWidth={480}
+    >
+      <Avatar name={`${firstName} ${lastName}`} size="xl" src={image} />
+      <GridItem area="info">
+        <Heading as="p" size="md" mb={2}>
+          {firstName} {lastName}
+        </Heading>
+        {nickname && (
+          <Heading as="p" size="sm" mb={2} sx={styles.nickname} fontStyle="oblique" color="gray.700">
+            <Text as="span">{nickname}</Text>
+          </Heading>
+        )}
+        <Text fontSize="sm" color="gray.500">
+          {email}
+        </Text>
+      </GridItem>
+    </Grid>
+  );
+}
+
+export default ProfileCard;
