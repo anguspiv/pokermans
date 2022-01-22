e8fb6e4ba6148c9d706287f401d3564a6de848ed
Angus <angusp@angusp.com>
2022-01-21 20:25:14 -0800

    feat: adds the page layout component
    Adds the component for PageLayouts to add global components to all pages
    
    re #52

---
+++ src/components/layout/PageLayout/PageLayout.stories.tsx
@@ -0,0 +1,24 @@
+import { SessionProvider } from 'next-auth/react';
+import { Flex } from '@chakra-ui/react';
+import PageLayout from './PageLayout';
+
+export default {
+  title: 'components/layout/PageLayout',
+  component: PageLayout,
+  parameters: {
+    layout: 'fullscreen',
+  },
+};
+
+const Template = (args) => (
+  <SessionProvider session={{}}>
+    <PageLayout {...args}>
+      <Flex width="100%" height="100%" justify="center" alignContent="center" background="red.100">
+        Content
+      </Flex>
+    </PageLayout>
+  </SessionProvider>
+);
+
+export const Default = Template.bind({});
+Default.args = {};
