f324696c82b5d4e1f9738315c1ea9479374d0314
Angus <angusp@angusp.com>
2022-01-23 19:15:10 -0800

    feat: adds the page header component
    Adds the PageHeader component to be displayed on internal pages
    
    closes #75

---
+++ src/components/layout/PageHeader/PageHeader.stories.tsx
@@ -0,0 +1,16 @@
+import { Story } from '@storybook/react';
+import PageHeader, { PageHeaderProps } from './PageHeader';
+
+export default {
+  title: 'components/layout/PageHeader',
+  component: PageHeader,
+};
+
+const Template: Story<PageHeaderProps> = (args) => <PageHeader {...args} />;
+
+export const Default = Template.bind({});
+
+Default.args = {
+  title: 'Test Title',
+  subtitle: 'Test Subtitle',
+};
