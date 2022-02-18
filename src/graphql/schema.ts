a659dd0cd3c7b1ab1b96117ee0962dd509f5ccf0
Angus <angusp@angusp.com>
2022-02-18 15:27:30 -0800

    feat(api): requires user login to access apis
    Requires an aunthenticated account to access all apis
    
    re #84

--- src/graphql/schema.ts
+++ src/graphql/schema.ts
@@ -6,11 +6,11 @@ export const schema = makeSchema({
   types,
   outputs: {
     typegen: join(process.cwd(), 'node_modules/@types/nexus-typegen/index.d.ts'),
-    schema: join(process.cwd(), 'graphql/schema.graphql'),
+    schema: join(process.cwd(), 'src/graphql/schema.graphql'),
   },
   contextType: {
     export: 'Context',
-    module: join(process.cwd(), 'graphql/context.ts'),
+    module: join(process.cwd(), 'src/graphql/context.ts'),
   },
 });
 
