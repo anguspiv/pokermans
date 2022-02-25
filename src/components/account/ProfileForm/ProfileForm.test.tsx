e62eae5031bccc3c440dee6be035fe3797002997
Angus <angusp@angusp.com>
2022-02-24 22:44:26 -0800

    feat: adds the profile form component
    Adds the profile form component

---
+++ src/components/account/ProfileForm/ProfileForm.test.tsx
@@ -0,0 +1,87 @@
+import React from 'react';
+import { render, fireEvent, waitFor } from '@testing-library/react';
+import { ProfileForm } from './ProfileForm';
+
+describe('<ProfileForm />', () => {
+  const setupProfileForm = (props) => {
+    return render(<ProfileForm {...props} />);
+  };
+
+  it('should render the profile form', () => {
+    expect.assertions(1);
+
+    const { getByTestId } = setupProfileForm();
+
+    expect(getByTestId('profile-form')).toBeInTheDocument();
+  });
+
+  it('should render the submit button', () => {
+    expect.assertions(1);
+
+    const { getByRole } = setupProfileForm();
+
+    expect(getByRole('button', { name: 'Save' })).toBeInTheDocument();
+  });
+
+  it('should display the first name field', () => {
+    expect.assertions(1);
+
+    const { getByLabelText } = setupProfileForm();
+
+    expect(getByLabelText('First Name')).toBeInTheDocument();
+  });
+
+  it('should display the firstName placeholder', () => {
+    expect.assertions(1);
+
+    const { getByPlaceholderText } = setupProfileForm();
+
+    expect(getByPlaceholderText('John')).toBeInTheDocument();
+  });
+
+  it('should set the default value for the firstName', () => {
+    expect.assertions(1);
+
+    const profile = {
+      firstName: 'John',
+    };
+
+    const { getByDisplayValue } = setupProfileForm({ profile });
+
+    expect(getByDisplayValue('John')).toBeInTheDocument();
+  });
+
+  it('should require the firstName value', async () => {
+    expect.assertions(1);
+
+    const { getByText, getByLabelText } = setupProfileForm();
+
+    fireEvent.blur(getByLabelText('First Name'));
+
+    await waitFor(() => expect(getByText('First Name is required')).toBeInTheDocument());
+  });
+
+  it('should change the value of the input', () => {
+    expect.assertions(1);
+
+    const { getByDisplayValue, getByLabelText } = setupProfileForm();
+
+    fireEvent.change(getByLabelText('First Name'), { target: { value: 'Jane' } });
+
+    expect(getByDisplayValue('Jane')).toBeInTheDocument();
+  });
+
+  it('should submit the values', async () => {
+    expect.assertions(2);
+
+    const onSubmit = jest.fn();
+
+    const { getByLabelText, getByRole } = setupProfileForm({ onSubmit });
+
+    fireEvent.change(getByLabelText('First Name'), { target: { value: 'Jane' } });
+
+    fireEvent.click(getByRole('button', { name: 'Save' }));
+
+    await waitFor(() => expect(onSubmit).toHaveBeenCalledWith({ firstName: 'Jane' }));
+  });
+});
---
+++ src/components/account/ProfileForm/ProfileForm.tsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import { FormControl, FormLabel, Input, Button, FormErrorMessage } from '@chakra-ui/react';
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from 'yup';
+
+export interface ProfileData {
+  firstName?: string;
+}
+
+export interface ProfileFormProps {
+  profile?: ProfileData;
+  onSubmit: (data: ProfileData) => void;
+}
+
+const schema = yup.object().shape({
+  firstName: yup.string().required('First Name is required'),
+});
+
+export function ProfileForm({ profile, onSubmit = () => {} }: ProfileFormProps) {
+  const {
+    handleSubmit,
+    register,
+    formState: { errors },
+  } = useForm({
+    mode: 'onBlur',
+    defaultValues: {
+      firstName: profile?.firstName || '',
+    },
+    resolver: yupResolver(schema),
+  });
+
+  const onFormSubmit = (data) => {
+    onSubmit(data);
+  };
+
+  const firstName = register('firstName');
+
+  return (
+    <form data-testid="profile-form" onSubmit={handleSubmit(onFormSubmit)}>
+      <FormControl isInvalid={errors.firstName}>
+        <FormLabel htmlFor="firstName">First Name</FormLabel>
+        <Input id="firstName" type="text" placeholder="John" {...firstName} />
+        <FormErrorMessage>{errors?.firstName?.message}</FormErrorMessage>
+      </FormControl>
+      <Button type="submit" mt={2}>
+        Save
+      </Button>
+    </form>
+  );
+}
+
+export default ProfileForm;
