7c5b6b4202457688a02cf53e3b74f86fd0264005
Angus <angusp@angusp.com>
2022-02-24 22:44:26 -0800

    feat: adds the profile form component
    Adds the profile form component

---
+++ src/components/account/ProfileForm/ProfileForm.stories.tsx
@@ -0,0 +1,21 @@
+import { Story } from '@storybook/react';
+import { ProfileForm, ProfileFormProps } from './ProfileForm';
+
+export default {
+  title: 'components/account/ProfileForm',
+  component: ProfileForm,
+  argTypes: { onSubmit: { action: 'submitted' } },
+};
+
+const Template: Story<ProfileFormProps> = ({ firstName, ...args }) => {
+  const profile = {
+    firstName,
+  };
+
+  return <ProfileForm {...args} profile={profile} />;
+};
+
+export const Default = Template.bind({});
+Default.args = {
+  firstName: 'John',
+};
