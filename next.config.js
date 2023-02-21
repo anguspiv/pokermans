fe5ea8b0b548876bf70e839bd17941ee714afd30
Angus <angusp@angusp.com>
2023-02-20 20:19:58 -0800

    fix(sentry): correctly setups the sentry integration
    fixes #92

--- next.config.js
+++ next.config.js
@@ -1,3 +1,15 @@
-module.exports = {
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const { withSentryConfig } = require('@sentry/nextjs');
+
+const moduleExports = {
   reactStrictMode: true,
+  sentry: {
+    hideSourceMaps: false,
+  },
 };
+
+const sentryWebpackPluginOptions = {
+  silent: true,
+};
+
+module.exports = withSentryConfig(moduleExports, sentryWebpackPluginOptions);
