08bbfaeae473a378246bfa05452c2752108a0cad
Angus <angusp@angusp.com>
2023-03-18 19:56:56 -0700

    feat(theme): sets primary color to teal
    Sets the primary color to teal for the App
    
    re #143

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,5 +1,14 @@
 import { createTheme, responsiveFontSizes } from '@mui/material/styles';
+import { teal } from '@mui/material/colors';
 
-const theme = responsiveFontSizes(createTheme());
+const theme = responsiveFontSizes(
+  createTheme({
+    palette: {
+      primary: {
+        main: teal[500],
+      },
+    },
+  }),
+);
 
 export default theme;
