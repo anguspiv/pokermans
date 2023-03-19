9de7dce59c8bd7461f1c994e6124691a7b727734
Angus <angusp@angusp.com>
2023-03-18 19:53:23 -0700

    fix(app-header): corrects the app-header styles
    Aligns the App Header links and buttons correctly,
    and inherits the correct styles

--- src/components/organisms/AppHeader/AppHeader.tsx
+++ src/components/organisms/AppHeader/AppHeader.tsx
@@ -5,35 +5,35 @@ import MenuOpenIcon from '@mui/icons-material/MenuOpen';
 import { AccountLink } from '@components/molecules/AccountLink';
 
 export interface AppHeaderProps {
-  isMenuOpen?: boolean;
+  open?: boolean;
   onMenuToggle?: () => void;
 }
 
 const OPEN_LABEL = 'Open Menu';
 const CLOSE_LABEL = 'close menu';
 
-function AppHeader({ isMenuOpen = false, onMenuToggle = () => {} }: AppHeaderProps) {
+function AppHeader({ open = false, onMenuToggle = () => {} }: AppHeaderProps) {
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="fixed" component="header" data-testid="app-header">
         <Toolbar sx={{ display: 'flex', alignItems: 'center' }}>
-          <Box sx={{ flexGrow: 1 }}>
+          <Box sx={{ flexGrow: 1, display: 'flex', alignItems: 'center' }}>
             <IconButton
               color="inherit"
-              aria-label={isMenuOpen ? CLOSE_LABEL : OPEN_LABEL}
+              aria-label={open ? CLOSE_LABEL : OPEN_LABEL}
               onClick={onMenuToggle}
               edge="start"
               data-testid="menu-button"
               sx={{ mr: 2 }}
             >
-              {isMenuOpen ? <MenuOpenIcon /> : <MenuIcon />}
+              {open ? <MenuOpenIcon /> : <MenuIcon />}
             </IconButton>
             <Typography variant="h6" noWrap component="div">
               PokerMans
             </Typography>
           </Box>
-          <Box sx={{ display: 'flex', alignItems: 'center' }}>
-            <AccountLink />
+          <Box sx={{ display: 'flex', alignItems: 'center', flex: '0 0 auto' }}>
+            <AccountLink color="inherit" />
           </Box>
         </Toolbar>
       </AppBar>
